0c3c7234d7789f2c0ba5d3e3298db0c950d4b019
Harshit Agarwal <harshiit.agarwall@gmail.com>
2018-01-28 11:07:46 +0530

    Splash screen modified

--- app/src/main/java/com/example/myapplication/imoxford/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/imoxford/MainActivity2.java
@@ -11,7 +11,6 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
         Intent in = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(in);
 
